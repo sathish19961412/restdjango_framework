eea6adcd3a8452ccd67b7756857d7843586a8d06
sathish19961412 <sathishfreelanc5@gmail.com>
2024-11-22 17:55:16 +0530

    create List specifi id api in drinks

--- drinks/urls.py
+++ drinks/urls.py
@@ -19,5 +19,6 @@ from django.urls import path
 from drinks import views
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('drinks/',views.drink_list)
+    path('drinks/',views.drink_list),
+    path('drinks/<int:id>',views.drink_detail)
 ]
--- drinks/views.py
+++ drinks/views.py
@@ -11,10 +11,28 @@ def drink_list(request):
     if request.method == 'GET':
         drinks = Drinks.objects.all()
         serializer=DrinkSerializer(drinks,many=True)
-        return JsonResponse({'drinks':serializer.data})
+        return Response({'drinks':serializer.data})
     
     if request.method =='POST':
         serializer=DrinkSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data,status=status.HTTP_201_CREATED)
+        
+@api_view(['GET','PUT','DELETE'])
+def drink_detail(request,id):
+
+    try:
+        drink=Drinks.objects.get(pk=id)
+    except Drinks.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    
+    if  request.method == 'GET':
+        serializer = DrinkSerializer(drink)
+        return Response(serializer.data)
+    
+    elif request.method == 'POST':
+        pass
+
+    elif request.method == 'DELETE':
+        pass
