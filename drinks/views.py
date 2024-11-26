aa30f275e5dc2d112a5ea6dd33d9a3e06a9f9e99
sathish19961412 <sathishfreelanc5@gmail.com>
2024-11-26 11:07:29 +0530

    compeleted to the model serializer api to crud operations"

--- drinks/views.py
+++ drinks/views.py
@@ -6,12 +6,12 @@ from rest_framework.response import Response
 from rest_framework import status
 
 @api_view(['GET','POST'])
-def drink_list(request):
+def drink_list(request,format=None):
 
     if request.method == 'GET':
         drinks = Drinks.objects.all()
         serializer=DrinkSerializer(drinks,many=True)
-        return Response({'drinks':serializer.data})
+        return Response(serializer.data)
     
     if request.method =='POST':
         serializer=DrinkSerializer(data=request.data)
@@ -20,7 +20,7 @@ def drink_list(request):
             return Response(serializer.data,status=status.HTTP_201_CREATED)
         
 @api_view(['GET','PUT','DELETE'])
-def drink_detail(request,id):
+def drink_detail(request,id,format=None):
 
     try:
         drink=Drinks.objects.get(pk=id)
@@ -30,9 +30,12 @@ def drink_detail(request,id):
     if  request.method == 'GET':
         serializer = DrinkSerializer(drink)
         return Response(serializer.data)
-    
-    elif request.method == 'POST':
-        pass
-
+    elif request.method == 'PUT':
+        serializer=DrinkSerializer(drink,data=request.data)
+        if serializer.is_valid():
+           serializer.save()
+           return Response(serializer.data)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
     elif request.method == 'DELETE':
-        pass
+        drink.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
